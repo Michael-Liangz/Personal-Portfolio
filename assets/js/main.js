799f0842ec2770aeb8ee5d2bbd97c97f27843596
Michael-Liangz <Michaelliangz.ml@gmail.com>
2022-12-13 20:15:21 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,34 @@ function linkAction() {
 navLink.forEach((list_item) => list_item.addEventListener("click", linkAction));
 
 // TODO ======================= ACCORDION SKILLS =======================
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
+}
+// ! ClickSkillsName -> kumpulan skill, seperti: Frontend developer, Backend developer dan Designer
+skillsHeader.forEach((ClickSkillsName) => {
+  ClickSkillsName.addEventListener("click", toggleSkills);
+});
 
 // TODO ======================= QUALIFICATION TABS =======================
 
 // TODO ======================= SERVICES MODAL =======================
+
+// TODO ======================= PORTFOLIO SWIPER =======================
+
+// TODO ======================= SERVICES MODAL =======================
+
+// TODO ======================= TESTIMONIAL =======================
+
+// TODO ======================= SCROLL SECTIONS ACRTIVE LINK =======================
+
+// TODO ======================= CHANGE BACKGROUND HEADER =======================
