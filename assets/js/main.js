d05917ab7e588149ca3a54102e9b213c8b3f0a74
Michael-Liangz <Michaelliangz.ml@gmail.com>
2022-12-20 21:10:10 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,6 +150,16 @@ function scrollActive() {
 window.addEventListener("scroll", scrollActive);
 
 // TODO ======================= CHANGE BACKGROUND HEADER =======================
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // * When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
 
 // TODO ======================= SHOW SCROLL TOP =======================
 
